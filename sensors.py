af39cc269a1a6e44b1f9919200744a9314296187
fortune1991 <172883547+fortune1991@users.noreply.github.com>
2025-09-09 14:54:33 +0100

    new temperature sensor added for external readings

--- sensors.py
+++ sensors.py
@@ -1,21 +1,22 @@
 from breakout_bme280 import BreakoutBME280
 from breakout_ltr559 import BreakoutLTR559
+from logging import system_log
 from pimoroni_i2c import PimoroniI2C
 from pimoroni import PICO_EXPLORER_I2C_PINS
 from machine import Pin, ADC
 from moisture import Moisture
 from time import sleep
+import onewire
+import ds18x20
 
 def sensor():
     """Set-up hardware connections"""
     i2c = PimoroniI2C(**PICO_EXPLORER_I2C_PINS) #explorer base
     bme = BreakoutBME280(i2c, address=0x76) # temp and rh
     ltr = BreakoutLTR559(i2c) # Lux
-    pi_temp = ADC(4) # The internal temperature sensor is connected to ADC4
+    external_thermometer = Pin(0, Pin.IN)
+    one_wire_sensor = ds18x20.DS18X20(onewire.OneWire(external_thermometer)) # also part of external temperature sensor
     moisture = Moisture(5) # Pin number
-    
-    # Convert raw ADC reading into voltage
-    temp_conversion_factor = 3.3 / 65535
     
     """Breakout Sensor Readings"""
     # TEMPERATURE AND RH INSIDE
@@ -26,10 +27,8 @@ def sensor():
     lux = get_lux(ltr)
     
     # EXTERNAL TEMP
-    # read the Pi sensor
-    outside_temp_reading = pi_temp.read_u16() * temp_conversion_factor
-    # Formula from the RP2040 datasheet:
-    temp_celc_outside = 27 - (outside_temp_reading - 0.706)/0.001721
+    # read the external thermometer
+    temp_celc_outside = get_external_temp(one_wire_sensor)
     
     # Moisture
     moisture_value = get_moisture(moisture)
@@ -69,86 +68,26 @@ def get_temp(bme, no_reads=3, delay=0.1):
         return temp_celc, pressure, rh
     return None
 
+def get_external_temp(one_wire_sensor, delay=0.1):
+    external_temp = 0
+    try:
+        roms = one_wire_sensor.scan()
+        sensor_count = len(roms)
+        if sensor_count > 0:
+            one_wire_sensor.convert_temp()
+            sleep(0.8)
+            for rom in roms:
+                external_temp = one_wire_sensor.read_temp(rom)
+                sleep(delay)
+
+    except:
+        system_log("Error reading external tempurature sensor")
+        
+    return external_temp
+
 def get_moisture(moisture):
     # give it at least 1 second to count pulses
     sleep(1.2)
     return moisture.read()
-
     
-    """
-    Sensor Connections Old
-    
-    import dht
-
-    # DHT Inside
-    dht_pin = Pin(0)
-    dht_sensor = dht.DHT22(dht_pin)
-
-    # DHT Outside
-    dht_outside_pin = Pin(1)
-    dht_outside_sensor = dht.DHT22(dht_outside_pin)
-
-    # LDR
-    ldr_pin = Pin(26)
-    ldr_sensor = ADC(ldr_pin)
-
-    # Soil Moisture Sensor
-    # ADD CODE HERE
-
-    # Constants for LDR config
-    GAMMA = 0.7
-    RL10 = 50000.0
-    R_FIXED = 10000.0  # 10 kΩ
-    """
-    
-    """
-    DHT Inside Sensor Reading
-
-    try:
-        dht_sensor.measure()
-
-        temp_celc = dht_sensor.temperature()
-        rh = dht_sensor.humidity()
-
-        #print("Temperature: {:.2f} °C".format(temp_celc))
-        #print("Humidity: {:.2f} %".format(rh))
-
-    except Exception as e:
-        print("Error reading DHT22:", str(e))
-    """
-
-    """
-    DHT Outside Sensor Reading
-
-    try:
-        dht_outside_sensor.measure()
-
-        temp_celc_outside = dht_outside_sensor.temperature()
-        rh_outside = dht_outside_sensor.humidity()
-
-        #print("Temperature: {:.2f} °C".format(temp_celc_outside))
-        #print("Humidity: {:.2f} %".format(rh_outside))
-
-    except Exception as e:
-        print("Error reading DHT22:", str(e))
-    """
-
-    """LDR Reading
-    try:
-        ldr_sensor_value = ldr_sensor.read_u16()  # 0-65535 for 0-3.3V
-        analogValue = ldr_sensor_value
-        voltage = analogValue * 3.3 / 65535.0
-        resistance = R_FIXED * voltage / (3.3 - voltage)
-
-        # Check for valid resistance
-        if resistance > 0:
-            lux = pow((RL10 * pow(10, GAMMA)) / resistance, 1.0 / GAMMA)
-            #print("Light Intensity: {:.2f} Lux".format(lux))
-        else:
-            print("Invalid resistance value, cannot compute lux.")
-
-    except Exception as e:
-        print("Error reading LDR:", str(e))
-    """
-
 
